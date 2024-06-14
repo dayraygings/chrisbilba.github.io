84a6fa0b2cae6cc70d1aaf276235f37a860a0a04
dayraygings <158171781+dayraygings@users.noreply.github.com>
2024-06-14 21:57:23 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Code pour les animations ou interactions supplémentaires
+    const sections = document.querySelectorAll('section');
+    const options = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+        section.classList.add('fade-out');
+    });
 });
